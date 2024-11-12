f7a9ea5f038f81f523cf21ee9fabfe9a84c10096
devnaiara <devnaiara@outlook.com>
2024-11-11 23:55:10 -0300

    primeiro commit

---
+++ src/conta/Menu.java
@@ -0,0 +1,129 @@
+package conta;
+
+import conta.controller.ContaController;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.io.IOException;
+
+public class Menu {
+    public static void main(String[] args) {
+        
+    	ContaController contas = new ContaController();
+    	
+        Scanner leia = new Scanner(System.in);
+		
+        int opcao;
+				
+        while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+			
+			try {
+				opcao = leia.nextInt();
+			}catch(InputMismatchException e){
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao=0;
+			}
+
+			if (opcao == 9) {
+				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
+				sobre();
+                 		leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println("\n Criar Conta");
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println("\n Listar todas as Contas");
+				contas.listarTodas();
+				keyPress();
+				break;
+			case 3:
+				System.out.println("\n Buscar Conta por número");
+
+				keyPress();
+				break;
+			case 4:
+				System.out.println("\n Atualizar dados da Conta");
+
+				keyPress();
+				break;
+			case 5:
+				System.out.println("\n Apagar Conta");
+
+				keyPress();
+				break;
+			case 6:
+				System.out.println("\n Sacar");
+
+				keyPress();
+				break;
+			case 7:
+				System.out.println("\n Depositar");
+
+				keyPress();
+				break;
+			case 8:
+				System.out.println("\n Transferir");
+
+				keyPress();
+				break;
+			default:
+				System.out.println("\nOpção Inválida" + Cores.TEXT_RESET);
+				
+				keyPress();
+				break;
+			}
+		}
+	}
+
+    	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Naiara Costa ");
+		System.out.println("Generation Brasil - devnaiara@gmail.com");
+		System.out.println("github.com/devnaiara");
+		System.out.println("*********************************************************");
+	}
+    
+	public static void keyPress() {
+
+		try {
+
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
+}
